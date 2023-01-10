8f17591d095894600b0b712b419a8edabc9c288b
Carlos Infante <kuva5008@gmail.com>
2023-01-10 06:15:37 +0100

    agrparser working yet bug

--- superpy.py
+++ superpy.py
@@ -12,15 +12,47 @@ __human_name__ = "superpy"
 
 # Your code below this line.
 def main():
-    print('BOUGHT \n:', data_bought)
-    print('SOLD \n:', data_sold)
-    print(items_details('graves'))
-    print(items_details('orange').fresh_item())
+    # print('BOUGHT \n:', data_bought)
+    # print('SOLD \n:', data_sold)
+    print(items_details(args.item))
+    print(items_details(args.yesterday).inventory_yesterday())
+    print(items_details(args.today).inventory_today())
+    print(items_details(args.storage).in_storage())
+    print(items_details(args.revenue_before_yesterday).revenue('before_yesterday'))
+    print(items_details(args.revenue_yesterday).revenue('yesterday'))
+    print(items_details(args.revenue_today).revenue())
+    print(items_details(args.fresh_product).fresh_item())
 
 
 path = os.getcwd()
 path_to_file_bought = os.path.join(path, "bought.csv")
 path_to_file_sold = os.path.join(path, "sold.csv")
+
+argParser = argparse.ArgumentParser(
+    prog='My Program',
+    description='Manage the inventory of the store',
+    epilog='Text at the bottom of help')
+
+# Parse arguments
+argParser.add_argument(
+    "-i", "--item", help="input an item")
+argParser.add_argument(
+    "-y", "--yesterday", help="inventory yesterday")
+argParser.add_argument(
+    "-t", "--today", help="inventory today")
+argParser.add_argument(
+    "-s", "--storage", help="Storage")
+argParser.add_argument(
+    "-r2", "--revenue_before_yesterday", help="Revenues before yesterday")
+argParser.add_argument(
+    "-r1", "--revenue_yesterday", help="Revenues yesterday")
+argParser.add_argument(
+    "-r", "--revenue_before_today", help="Revenues before today")
+argParser.add_argument(
+    "-f", "--fresh_product", help="Type an item to check if it is not outdated")
+
+args = argParser.parse_args()
+
 
 # Storing the data of the .csv files in variables
 data_bought = pd.read_csv(path_to_file_bought)
@@ -52,6 +84,28 @@ class Product():
         x = sum([self.sold_before_yesterday,
                 self.sold_yesterday, self.sold_today])
         return x
+
+    # subclass inventory yesterday
+    def inventory_yesterday(self):
+        dic_yesterday = {'item': [self.item],
+                         'count': [self.sold_yesterday],
+                         'Buy price': [self.price],
+                         'Expiration Day': [self.expiration_date]}
+        df = pd.DataFrame(dic_yesterday)
+        x = df.to_csv()
+        return (x)
+
+
+# subclass inventory today
+
+    def inventory_today(self):
+        dic_today = {'item': [self.item],
+                     'count': [self.sold_today],
+                     'Buy price': [self.price],
+                     'Expiration Day': [self.expiration_date]}
+        df = pd.DataFrame(dic_today)
+        x = df.to_csv()
+        return (x)
 
     # Subclass inventory. What it remains in the storage
     def in_storage(self):
